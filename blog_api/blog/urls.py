3b61a1262e174b3a08bc6b37fab1f3de2efb4ed4
angelgracecy <angelgracecy@gmail.com>
2024-10-19 06:56:26 +0000

    new

---
+++ blog_api/blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, CategoryViewSet, TagViewSet, BlogPostViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'tags', TagViewSet)
+router.register(r'posts', BlogPostViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
